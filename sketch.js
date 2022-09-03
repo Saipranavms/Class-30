280ecb93f10cd50ec7b6003a5e2c14c13a8d3962
Saipranavms <107045415+Saipranavms@users.noreply.github.com>
2022-09-03 21:24:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,72 +9,109 @@ const Composite = Matter.Composite;
 
 let engine;
 let world;
-var ground;
-var fruit,rope;
-var fruit_con;
+var ground, bridge;
+var leftWall, rightWall;
+var jointPoint;
+var jointLink;
+var zombie;
+var zombie1, zombie2, zombie3, zombie4;
+var breakButton;
+var backgroundImage;
 
-var bg_img;
-var food;
-var rabbit;
-var button
+var stones = [];
 
-function preload()
-{
-  bg_img = loadImage('background.png');
-  food = loadImage('melon.png');
-  rabbitImg = loadImage('Rabbit-01.png');
+function preload() {
+  zombie1 = loadImage("./assets/zombie1.png");
+  zombie2 = loadImage("./assets/zombie2.png");
+
+  zombie3 = loadImage("./assets/zombie3.png");
+  zombie4 = loadImage("./assets/zombie4.png");
+
+  backgroundImage = loadImage("./assets/background.png");
 }
 
-function setup() 
-{
-  createCanvas(500,700);
-  frameRate(80);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   engine = Engine.create();
   world = engine.world;
+  frameRate(80);
 
-  button = createImg('cut_button.png')
-  button.position(200,30)
-  button.size(50,50)
-  button.mouseClicked(drop)
-  
-  ground = new Ground(200,680,600,20);
+  ground = new Base(0, height - 10, width * 2, 20);
+  leftWall = new Base(100, height - 300, 200, height / 2 + 100);
+  rightWall = new Base(width - 100, height - 300, 200, height / 2 + 100);
 
-  rabbit = createSprite(200,620,100,100)
-  rabbit.addImage(rabbitImg)
-  rabbit.scale = 0.2
+  bridge = new Bridge(30, { x: 50, y: height / 2 - 140 });
+  jointPoint = new Base(width - 250, height / 2 - 100, 40, 20);
 
-  rope = new Rope(7,{x:245,y:30});
-  fruit = Bodies.circle(300,300,20);
-  Matter.Composite.add(rope.body,fruit);
+  Matter.Composite.add(bridge.body, jointPoint);
+  jointLink = new Link(bridge, jointPoint);
 
-  fruit_con = new Link(rope,fruit);
+  for (var i = 0; i <= 8; i++) {
+    var x = random(width / 2 - 200, width / 2 + 300);
+    var y = random(-100, 100);
+    var stone = new Stone(x, y, 80, 80);
+    stones.push(stone);
+  }
 
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
-  textSize(50)
-  imageMode(CENTER);
+  zombie = createSprite(width / 2, height - 110);
+  zombie.addAnimation("lefttoright", zombie1, zombie2, zombie1);
+  zombie.addAnimation("righttoleft", zombie3, zombie4, zombie3);
+  zombie.scale = 0.1;
+  zombie.velocityX = 10;
 
-  
+  breakButton = createButton("");
+  breakButton.position(width - 200, height / 2 - 50);
+  breakButton.class("breakbutton");
+
+  //breakButton.mouseClicked(handleButtonPress);
+  breakButton.mousePressed(handleButtonPress);
+  //breakButton.mouse(handleButtonPress);
+  //breakButton.mousePressed(ButtonPress);
+
+
 }
 
-function draw() 
-{
-  background(51);
- 
-
-  image(bg_img,width/2,height/2,490,690);
-
-  image(food,fruit.position.x,fruit.position.y,70,70);
-  rope.show();
+function draw() {
+  background(backgroundImage);
   Engine.update(engine);
-  ground.show();
 
- 
-  drawSprites()
+  bridge.show();
+
+  for (var stone of stones) {
+    stone.show();
+  }
+
+  if (zombie.position.x >= width - 300) {
+    zombie.velocityX = -10;
+    zombie.changeAnimation("righttoleft");
+  }
+
+  if (zombie.position.x <= 300) {
+    zombie.velocityX = 10;
+    zombie.changeAnimation("lefttoright");
+  }
+
+  drawSprites();
 }
 
-function drop(){
-rope.break()
-fruit_con.detatch()
-fruit_con=null
+function handleButtonPress() {
+  /* jointLink=dettach();
+  setTimeout(() => {
+    bridge.break();
+  }, 1500); */
+
+  /* jointLink.dettach();
+  setTimeout(() => {
+    break();
+  }, 1500); */
+
+   jointLink.dettach();
+  setTimeout(() => {
+    bridge.break();
+  }, 5); 
+
+  /* jointLink.dettach();
+  setTimeout(() => {
+    bridge.break();
+  }, 1500); */
 }
